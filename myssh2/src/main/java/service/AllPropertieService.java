159cbee4c5713dd3ce7004db2ed47ea969105965
sam4lai <28248051@qq.com>
2020-04-17 09:52:50 -0400

    -

--- myssh2/src/main/java/service/AllPropertieService.java
+++
@@ -1,8 +0,0 @@
-package service;
-import java.util.List;
-
-import entity.Tallproperties;;
-
-public interface AllPropertieService {
-	public List<Tallproperties> getPropertyByGroup(String group);
-}
